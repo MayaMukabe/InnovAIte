1d94b8f6e100c93333e28dae1e4cd1f86dac2556
abdulahi-banji <abdulahioyebanji01@gmail.com>
2026-07-25 22:36:08 -0400

    created file

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>Hero Academy</h1>
+      <p>Independent thinking is your superpower.</p>
+    </main>
+  )
+}
+
+export default App
